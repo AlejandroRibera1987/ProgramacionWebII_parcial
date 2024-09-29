6c527be2c4eaa6e62cf20230ce49a71d2a2669a1
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-09-29 20:46:54 -0300

    Actualizacion Cursos y Clientes

--- page/cursos.php
+++ page/cursos.php
@@ -2,6 +2,20 @@
     include_once ('../components/header.php');
 ?>
 
+    <main>
+        <div class="portada_cursos">
+            <figure>
+                <img src="../img/cursos_1.jpg" alt="Portada Cursos">
+            </figure>
+            <div class="texto_portada_cursos">
+                <h2>Encontra los mejores cursos para tu negocio</h2>
+                <a href="contacto.php">Contactanos</a>
+            </div>
+            <figure>
+                <img src="../img/cursos_2.jpg" alt="Portada Cursos">
+            </figure>
+        </div>
+    </main>
 
 <?php
     include_once ('../components/footer.php');
