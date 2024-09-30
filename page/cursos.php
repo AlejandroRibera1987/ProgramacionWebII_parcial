9205bd672623b48ec11eaa40649f14694db02522
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-09-30 10:27:31 -0300

    Actualizacion Section Cursos

--- page/cursos.php
+++ page/cursos.php
@@ -15,6 +15,56 @@
                 <img src="../img/cursos_2.jpg" alt="Portada Cursos">
             </figure>
         </div>
+
+        <div class="cursos_main">
+            <h2>Cursos que tenemos para tu negocio</h2>
+            <div class="table_cursos">
+                <div class="card_cursos">
+                    <figure>
+                        <img src="../img/Curso_MarketinDigital.jpg" alt="Marketin Digital">
+                        <figcaption>$1500</figcaption>
+                    </figure>
+                    <div class="descripcion_curso">
+                        <h2>Marketin Digital</h2>
+                        <p>Ofrecemos una introducción completa a las estrategias y herramientas clave para promocionar productos y servicios en el entorno digital. Aprenderás sobre SEO, SEM, redes sociales, email marketing, analítica web y creación de contenido</p>
+                        <a href="contacto.php">Contratar</a>
+                    </div>
+                </div>
+                <div class="card_cursos">
+                    <figure>
+                        <img src="../img/Curso_liderazgo.jpg" alt="Liderazgo Curso">
+                        <figcaption>$2588</figcaption>
+                    </figure>
+                    <div class="descripcion_curso">
+                        <h2>Desarrollo de Liderazgo</h2>
+                        <p>El curso de Desarrollo de Habilidades de Liderazgo está diseñado para mejorar las capacidades de gestión y liderazgo. A través de técnicas de comunicación efectiva, toma de decisiones, motivación de equipos y resolución de conflictos. </p>
+                        <a href="contacto.php">Contratar</a>
+                    </div>
+                </div>
+                <div class="card_cursos">
+                    <figure>
+                        <img src="../img/curso_getionProyecto.jpg" alt="Curso de Gestion de Proyectos">
+                        <figcaption>$3250</figcaption>
+                    </figure>
+                    <div class="descripcion_curso">
+                        <h2>Gestión de Proyectos</h2>
+                        <p>El Curso te enseña a planificar, ejecutar y supervisar proyectos de manera eficiente. Aprenderás sobre metodologías, gestión de recursos, cronogramas y riesgos, con el objetivo de garantizar que los proyectos se completen dentro del plazo y presupuesto establecidos.</p>
+                        <a href="contacto.php">Contratar</a>
+                    </div>
+                </div>
+                <div class="card_cursos">
+                    <figure>
+                        <img src="../img/Negoziacion.jpg" alt="Curso de Negoziacion">
+                        <figcaption>$5600</figcaption>
+                    </figure>
+                    <div class="descripcion_curso">
+                        <h2>Estrategias de Ventas y Negociación</h2>
+                        <p>El curso enseña técnicas para aumentar las ventas y cerrar acuerdos efectivos. Aprenderás a identificar oportunidades, manejar objeciones, negociar con éxito y crear relaciones comerciales sólidas. Ideal para profesionales que buscan mejorar sus habilidades de ventas y negociación en entornos competitivos.</p>
+                        <a href="contacto.php">Contratar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 
 <?php
